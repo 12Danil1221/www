35a520b906d83653a4b656215a834ef1997260ff
12Danil1221 <51080265+12Danil1221@users.noreply.github.com>
2025-08-28 14:21:05 +0500

    Load image db

--- func/add-game.php
+++ func/add-game.php
@@ -1,16 +1,21 @@
 <?php 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-$image = trim(htmlspecialchars($_POST['image'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
-$content = trim(htmlspecialchars($_POST['content'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
-$followers = trim(htmlspecialchars($_POST['followers'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+$upload_dir = $_SERVER['DOCUMENT_ROOT'].'/www/uploads/image/';
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $image_name = $upload_dir.$_FILES['image']['name'];
+        $tmpname = $_FILES["image"]["tmp_name"];
+    
+        $image = 'uploads/image/'.$_FILES['image']['name'];
+        $content = trim(htmlspecialchars($_POST['content'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+        $followers = trim(htmlspecialchars($_POST['followers'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
     }
 //DB
-$pdo = new PDO('mysql:host=localhost;dbname=php-website;port=3306', 'root', '');
+require('./db.php');
 
+move_uploaded_file($tmpname, $image_name);
 
-$sql = "INSERT INTO trending (image, content, followers) VALUES (?,?,?)";
-$query = $pdo->prepare($sql);
-$query->execute([$image,$content,$followers]);
+$query = new db();
+$query->setInsertQuery('INSERT INTO trending (image, content, followers) VALUES (?,?,?)', [$image,$content,$followers]);
 
 header('Location: ../add-game.php');
 
