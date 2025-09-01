6111b999e3b1ec74bd70e4907d3abf0a2c752e7d
12Danil1221 <51080265+12Danil1221@users.noreply.github.com>
2025-09-01 20:48:59 +0500

    Refactor: Simplify add-cart validation and database interaction

--- func/add-cart.php
+++ func/add-cart.php
@@ -5,40 +5,36 @@ $followers = trim(htmlspecialchars($_POST['followers'], FILTER_SANITIZE_SPECIAL_
 $content = trim(htmlspecialchars($_POST['content'], FILTER_SANITIZE_SPECIAL_CHARS));
     }
 
-$errors = [];
-if(strlen($image) < 3){
-    $error = "Image must be at least 3 characters long.";
-}
-if(strlen($followers) < 1){
-    $error = "Followers must be at least 1 character long.";
-}
-if(strlen($content) < 3){
-    $errors[] = "Content must be at least 3 characters long.";
-}
+// $errors = [];
+// if(strlen($image) < 3){
+//     $error = "Image must be at least 3 characters long.";
+// }
+// if(strlen($followers) < 1){
+//     $error = "Followers must be at least 1 character long.";
+// }
+// if(strlen($content) < 3){
+//     $errors = "Content must be at least 3 characters long.";
+// }
 
-if(empty($errors)){
-    // Insert data into database
-    foreach($errors as $error){
-        echo $error . "\n";
-    }
-    exit;
-}
+// if(empty($errors)){
+//     // Insert data into database
+//     foreach($errors as $error){
+//         echo $error . "\n";
+//     }
+//     exit;
+// }
 //DB
 require_once "../func/db.php";
-    // Проверка на дубликат
-    $stmt = $pdo->prepare("SELECT 1 FROM cart WHERE Image = :image");
-    $stmt->execute([
-        'image' => $image,
-    ]);
-    $result = $stmt->fetch(PDO::FETCH_NUM);
+$query = new db();
+    // Проверка на дубликат изображения товара
+    $result = $query->setSelectQuery('SELECT 1 FROM cart WHERE image = :image', [':image'=>$image]);
+    
     if ($result[0]) {
         header('Location: ../add-cart-null.php');
         exit;
     } else{
 //SQL
-$sql = 'INSERT INTO cart(image, followers, content) VALUES (?,?,?)';
-$query = $pdo->prepare($sql);
-$query->execute([$image, $followers, $content]);
+$query->setInsertQuery('INSERT INTO cart(image, followers, content) VALUES (?,?,?)', [$image, $followers, $content]);
 
 header('Location: ../trending.php');
     }
