0844b3ecc49d6186d485b4bac68d58b46d267b12
12Danil1221 <51080265+12Danil1221@users.noreply.github.com>
2025-09-16 13:03:52 +0500

    📝 Update user page: Refactor login display to utilize User class for improved code clarity and maintainability.

--- user.php
+++ user.php
@@ -10,15 +10,22 @@
 
 <body>
     <?php require_once "include/header.php" ?>
+    <?php require_once("./classes/User.php");
+    $user = new User($_COOKIE['login'] ?? 'Гость');
+    ?>
 
     <main class="feedback">
         <div class="container">
             <h2>Кабинет пользователя</h2>
-            <p>Привет: <b><?= $_COOKIE['login'] ? htmlspecialchars($_COOKIE['login']) : 'Гость' ?></b>.</p>
+            <p>Привет: <b><?= $user->getLogin() ?></b>.</p>
+
         </div>
     </main>
 
     <?php require_once "include/footer.php" ?>
+
+    <script>
+    </script>
 </body>
 
 </html>
