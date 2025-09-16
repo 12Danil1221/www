83c0ced7c921e0803e188b15dcd7437b767f601b
12Danil1221 <51080265+12Danil1221@users.noreply.github.com>
2025-09-16 13:03:17 +0500

    📝 Update index page content: Replace English text with Russian descriptions, enhance layout structure, and update images for a more cohesive user experience.

--- index.php
+++ index.php
@@ -1,41 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
+<div class="wrapper">
+    <?php require_once "include/header.php" ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Game Website</title>
-    <link rel="stylesheet" href="../www/css/main.css">
-</head>
 
-<body>
-    <div class="wrapper">
-        <?php require_once "include/header.php" ?>
-
-        <div class="hero container">
-            <div class="hero--info">
-                <h2>3D game Dev</h2>
-                <h1>Work that we produce for our clients</h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                    industry's standard.</p>
-                <button class="btn">Get more details</button>
-            </div>
-            <img src="   https://cdn-icons-png.flaticon.com/512/1334/1334203.png " style="top:-30px;" width="556"
-                height="556" alt="" title="" class="img-small">
+    <div class="hero container">
+        <div class="hero--info">
+            <h2>ТОВАР В ТВОЙ ДОМ</h2>
+            <h1>Мы создаём уютную и стильную мебель для вашего дома.</h1>
+            <p>Наша работа включает в себя дизайн высокого качества, использование натуральных материалов и
+                современных технологий, а также внимание к деталям для обеспечения максимального комфорта и гармонии
+                в вашем интерьере.</p>
+            <a href="#all-tovars" class="btn">Подробнее</a>
         </div>
+        <img src="./img/header-banner.jpg" width="506" height="606" alt="3D Game Icon" title="3D Game Icon"
+            class="img-small">
+    </div>
 
-        <section class="container trending">
-            <?php 
+
+    <section class="container trending">
+        <?php 
             if(isset($_COOKIE['login'])){
                 ?>
-            <a href="./trending.php" class="see-all">SEE ALL</a>
-            <?php
+        <a href="./trending.php" class="see-all">Все</a>
+        <?php
             }
             ?>
-            <h3>Currently Trending Games</h3>
+        <h3 id="all-tovars">Текущий популярный товар</h3>
 
-            <div class="games">
-                <?php
+        <div class="games">
+            <?php
                 //DB
                 require_once "./func/db.php";
                 $query = new db();
@@ -50,7 +42,7 @@
                         $followers = htmlspecialchars($el->followers, ENT_QUOTES, 'UTF-8');
                         $content = htmlspecialchars($el->content, ENT_QUOTES, 'UTF-8');
                         echo '
-                        <div class="block">
+                        <div class="block d-grid w-auto">
                             <img src="'.$el->image.'" alt="Image" width="250" height="250">
                             <span><img src="./img/fire.svg" alt=""> '.$el->followers.' Followers</span>
                             <span>'.$el->content.'</span>
@@ -60,123 +52,134 @@
                     echo "<p>Нет данных для отображения.</p>";
                 }
                 ?>
-            </div>
-        </section>
+        </div>
+    </section>
 
-        <section class="container big-text">
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                <br>
-                <?php
+    <section class="container big-text">
+        <p>Мебель вашей мечты — у нас вы найдёте идеальный вариант!
+            <br>
+            <?php
                 @$login = $_COOKIE['login'];
 
                  $result = $query->setSelectQuery('SELECT role FROM users WHERE login = ?',[$login]);
                 if(@$_COOKIE['login'] && $result[0]->role == 'admin'):
                     ?>
-                <a href="./add-game.php" style="color:white">Добавить товар</a>
-                <?php
-                endif;
-                 ?>
-            </p>
-        </section>
+            <a href="./add-game.php" style="color:white">Добавить товар</a>
+            <?php endif; ?>
+        </p>
+    </section>
 
-        <section class="container banner">
-            <h3>Lorem Ipsum</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s,</p>
-            <img src="img/banner.jpeg" alt="Banner" style="border-radius: 50px;">
-        </section>
-    </div>
+    <section class="container banner">
+        <h3>Акции и специальные предложения!</h3>
+        <p>Не упустите шанс обновить интерьер с нашей мебелью!</p>
+        <img src="img/main-banner.jpeg" alt="Banner" style="border-radius: 50px;">
+    </section>
+</div>
 
-    <section class="features">
-        <div class="container">
-            <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s,</p>
-            <div class="info">
-                <div class="block">
-                    <img src="img/feature1.png" alt="">
-                    <p>Mobile Game Development</p>
+<section class="features">
+    <div class="container">
+        <h3>Почему стоит выбрать нашу мебель?</h3>
+        <p>«Программа лояльности: выгодные предложения для наших клиентов»</p>
+        <div class="info">
+            <div class="block">
+                <img class="feature-image" src="img/feature-main1.png" alt="">
+                <p>Качество и долговечность.</p>
+                <div class="d-flex justify-content-end">
                     <img src="img/arrow.png" alt="">
                 </div>
-                <div class="block">
-                    <img src="img/feature2.png" alt="">
-                    <p>PC Game Development</p>
+            </div>
+            <div class="block">
+                <img class="feature-image" src="img/feature-main2.png" alt="">
+                <p>Стильный дизайн.</p>
+                <div class="d-flex justify-content-end">
                     <img src="img/arrow.png" alt="">
                 </div>
-                <div class="block">
-                    <img src="img/feature3.png" alt="">
-                    <p>PS4 Game Development</p>
+            </div>
+            <div class="block">
+                <img class="feature-image" src="img/feature-main3.png" alt="">
+                <p>Разнообразие выбора.</p>
+                <div class="d-flex justify-content-end">
                     <img src="img/arrow.png" alt="">
                 </div>
-                <div class="block">
-                    <img src="img/feature4.png" alt="">
-                    <p>AR/VR Solutions</p>
+            </div>
+            <div class="block">
+                <img class="feature-image" src="img/feature-main4.png" alt="">
+                <p>Индивидуальный подход.</p>
+                <div class="d-flex justify-content-end">
                     <img src="img/arrow.png" alt="">
                 </div>
-                <div class="block">
-                    <img src="img/feature5.png" alt="">
-                    <p>AR/ VR design</p>
+            </div>
+            <div class="block">
+                <img class="feature-image" src="img/feature5.png" alt="">
+                <p>Гарантия качества.</p>
+                <div class="d-flex justify-content-end">
                     <img src="img/arrow.png" alt="">
                 </div>
-                <div class="block">
-                    <img src="img/feature6.png" alt="">
-                    <p>3D Modelings</p>
+            </div>
+            <div class="block">
+                <img class="feature-image" src="img/feature6.png" alt="">
+                <p>Программа лояльности</p>
+                <div class="d-flex justify-content-end">
                     <img src="img/arrow.png" alt="">
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section class="wrapper">
-        <div class="container projects">
-            <h3>Our Recent Projects</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-            <div class="images">
-                <img src="img/Project1.png" alt="">
-                <img src="img/Project2.png" alt="">
-                <img src="img/Project3.png" alt="">
-            </div>
-            <div class="images">
-                <img src="img/Project4.png" alt="">
-                <img src="img/Project5.png" alt="">
-                <img src="img/Project6.png" alt="">
-            </div>
-            <?php 
+<section class="wrapper">
+    <div class="container projects">
+        <h3>Наши последние проекты</h3>
+        <p>Ознакомьтесь с нашими последними проектами по продаже мебели. Каждый предмет мебели разработан таким образом,
+            чтобы привнести красоту и функциональность в ваше жилое пространство. Изучите нашу коллекцию и найдите
+            идеальное дополнение к вашему дому.</p>
+        <div class="images">
+            <img src="img/project_1 (2).jpeg" alt="">
+            <img src="img/project_2 (2).jpeg" alt="">
+            <img src="img/project_3 (2).jpeg" alt="">
+        </div>
+        <div class="images">
+            <img src="img/project_4 (2).jpeg" alt="">
+            <img src="img/project_5 (2).jpeg" alt="">
+            <img src="img/project_6 (2).jpeg" alt="">
+        </div>
+        <?php 
             if(isset($_COOKIE['login'])){
             ?>
-            <a href="" class="see-all">SEE ALL</a>
-            <?php
+        <a href="./trending.php" class="see-all">ВСЕ</a>
+        <?php
             }
             ?>
-        </div>
+    </div>
 
-        <div class="container email">
-            <h3>Lorem Ipsum</h3>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-            <div class="block">
-                <div>
-                    <h4>Stay in the loop</h4>
-                    <p>Subscribe to receive the latest news and updates about TDA.
-                        We promise not to spam you! </p>
-                </div>
-                <div>
-                    <input type="email" id="emailField" placeholder="Enter email address">
-                    <button onclick="checkEmail()">Continue</button>
-                </div>
+    <div class="container email">
+        <h3>Подписывайтесь на уведомления</h3>
+        <p>Уведомления только о важных изменениях. </p>
+        <div class="block">
+            <div>
+                <h4>Будьте в курсе событий</h4>
+                <p>Подпишитесь на получение последних новостей и обновлений о нашем магазине. Мы обещаем не рассылать
+                    вам спам!
+                </p>
+            </div>
+            <div class="d-flex align-items-center">
+                <input type="email" id="emailField" placeholder="Enter email address">
+                <button onclick="checkEmail()">Continue</button>
             </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <?php require_once "include/footer.php" ?>
+<?php require_once "include/footer.php" ?>
 
-    <script>
-    function checkEmail() {
-        let email = document.querySelector('#emailField').value;
-        if (!email.includes('@')) alert('Нет символа @');
-        else if (!email.includes('.')) alert('Нет символа .');
-        else alert('Все отлично!');
-    }
-    </script>
+<script>
+function checkEmail() {
+    let email = document.querySelector('#emailField').value;
+    if (!email.includes('@')) alert('Нет символа @');
+    else if (!email.includes('.')) alert('Нет символа .');
+    else alert('Все отлично!');
+}
+</script>
 </body>
 
 </html>
